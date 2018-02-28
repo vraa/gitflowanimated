c0d1cf1bc50edcc0d35566ced39b0869a0ea92ed
Veera <veera.k.sundar@gmail.com>
2018-02-27 18:16:41 -0800

    Get auto drwaing line works.

--- src/connections.js
+++ src/connections.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
 
 const ConnectionsElm = styled.svg`
     position: absolute;
@@ -9,6 +9,25 @@ const ConnectionsElm = styled.svg`
     height: 100%;
     z-index: 10;
 `;
+
+const draw = keyframes`
+    to {
+        stroke-dashoffset: 0;   
+    }
+`;
+
+const AnimatedLine = styled.line`
+    stroke-dasharray: 500;
+    stroke-dashoffset: 500;
+    animation: ${draw} 2s linear forwards;
+`;
+
+const AnimatedPath = styled.path`
+    stroke-dasharray: 500;
+    stroke-dashoffset: 500;
+    animation: ${draw} 2s linear forwards;
+`;
+
 
 class Connections extends Component {
 
@@ -21,7 +40,7 @@ class Connections extends Component {
                         const {src, tgt} = path;
                         let elm = null;
                         if (src.left === tgt.left) {
-                            elm = <line
+                            elm = <AnimatedLine
                                 key={'p' + src.left + '-' + src.top + '-' + idx}
                                 x1={src.left} y1={src.top + 25} x2={tgt.left} y2={tgt.top}
                                 fill={'none'}
@@ -67,7 +86,7 @@ class Connections extends Component {
                             }
 
                             let pathStr = `M${p1.x},${p1.y} C${c1.x},${c1.y} ${c2.x},${c2.y} ${p2.x},${p2.y}`;
-                            elm = <path
+                            elm = <AnimatedPath
                                 key={'p' + src.left + '-' + src.top + '-' + idx}
                                 d={pathStr}
                                 fill={'none'}
